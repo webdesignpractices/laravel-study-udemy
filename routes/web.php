a6a3e8b00c0e8f88cf13025ac78ef8859da05acd
ar <officebusiness800@gmail.com>
2026-01-30 14:31:02 +0900

    共通部分のレイアウトをまとめるファイルを作り@yield,@sectionの使用方法を学んだ

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/hello-world',function(){
+//     return 'hello-world';
+// });
+Route::get('/hello-world',fn()=>
+    view('hello_world')
+);
+Route::get('/hello',fn()=>
+    view('hello',['name'=>'山田','course'=>'Laravel'])
+);
+Route::get('/',fn()=>view('index'));
+Route::get('/curriculum',fn()=>view('curriculum'));
