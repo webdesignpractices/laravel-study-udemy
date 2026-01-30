d3d95ca8b7b3f9338f63ee9103817593712cdcc9
ar <officebusiness800@gmail.com>
2026-01-30 15:09:16 +0900

    controllerを作成しルーティングの処理部分をメソッド内に記述した

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -17,3 +19,13 @@ Route::get('/hello',fn()=>
 );
 Route::get('/',fn()=>view('index'));
 Route::get('/curriculum',fn()=>view('curriculum'));
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class,'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class,'omikuji']);
+
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class,'montyHall']);
