8b2ff30b0ee87d1da0e661d0412a209a16c0e5d4
ar <officebusiness800@gmail.com>
2026-02-01 13:52:33 +0900

    resource controllerの作成コマンドとresource routeの記法と確認コマンドについて

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UtilityController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\RequestSampleController;
+use App\Http\Controllers\EventController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -39,4 +40,6 @@ Route::get('/route-link',[RequestSampleController::class,'routeLink']);
 
 Route::get('/login',[RequestSampleController::class,'loginForm']);
 Route::post('/login',[RequestSampleController::class,'login'])->name('login');
+
+Route::resource('/event',EventController::class)->only(['index','create','store']);
 
